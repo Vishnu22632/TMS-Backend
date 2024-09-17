565582e47711e53ee4b812214174a6f2c8b3e3c1
Vishnu <vishnu22632@gmail.com>
2024-09-17 12:04:49 +0545

    Completed implementation of REST API for Users entity

--- src/main/java/com/synergy/tms/controller/UserController.java
+++ src/main/java/com/synergy/tms/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.synergy.tms.entity.User;
 import com.synergy.tms.service.UserService;
 
+@CrossOrigin("http://localhost:5173/")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
